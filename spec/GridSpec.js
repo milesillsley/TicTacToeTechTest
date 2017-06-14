7fb93bde92788e97e2215f6f747c7c1aacb15e25
milesillsley <milesillsley@hotmail.co.uk>
2017-06-14 13:03:21 +0100

    passing tests: the first and second turns

--- spec/GridSpec.js
+++ spec/GridSpec.js
@@ -1,6 +1,6 @@
 (function() {
   describe('Grid', function() {
-    it('populates a blank 2 dimensional array', function() {
+    it('populates a blank 2 dimensional array with empty strings', function() {
       grid = new Grid(1);
       expect(grid).toEqual([['']]);
       grid2 = new Grid(2);
--- src/TicTacToe.js
+++ src/TicTacToe.js
@@ -2,9 +2,21 @@
 
 var TicTacToe = function(gameSize) {
   this._grid = new Grid(gameSize);
+  this._currentPlayer = 'X';
 };
-
 TicTacToe.prototype.move = function(x,y) {
-
+ this._grid[x][y] = this.getCurrentPlayer();
+ this.endTurn();
 };
-TicTacToe.prototype.getPos = function(x,y) {};
+TicTacToe.prototype.getPos = function(x,y) {
+  return this._grid[x][y];
+};
+TicTacToe.prototype.getCurrentPlayer = function() {
+  return this._currentPlayer;
+};
+TicTacToe.prototype.endTurn = function() {
+  this.switchPlayer();
+};
+TicTacToe.prototype.switchPlayer = function() {
+  this._currentPlayer === 'X' ? this._currentPlayer = 'O' : this._currentPlayer = 'X';
+};
